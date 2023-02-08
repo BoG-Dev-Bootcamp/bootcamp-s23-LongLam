49d370035f6b727b830695cce7bafad74490e8fe
llam36 <llam36@gatech.edu>
2023-02-07 19:14:52 -0500

    update exer4 with await and try/catch bolck

--- exer4/index.js
+++ exer4/index.js
@@ -2,16 +2,28 @@ import figlet from "figlet";
 import axios from 'axios';
 
 let endpoint = Math.floor(Math.random() * 1008 + 1);
-axios.get("https://pokeapi.co/api/v2/pokemon/"+endpoint)
-    .then(function (response) {
-        console.log(figlet.textSync(response.data.name, {
-            font: 'Caligraphy',
-            horizontalLayout: 'default',
-            verticalLayout: 'default',
-            width: 200,
-            whitespaceBreak: true
-        }));
-    })
-    .catch(function (error) {
-        console.log(error)
-    })
+// axios.get("https://pokeapi.co/api/v2/pokemon/"+endpoint)
+//     .then(function (response) {
+//         console.log(figlet.textSync(response.data.name, {
+//             font: 'Caligraphy',
+//             horizontalLayout: 'default',
+//             verticalLayout: 'default',
+//             width: 200,
+//             whitespaceBreak: true
+//         }));
+//     })
+//     .catch(function (error) {
+//         console.log(error)
+//     })
+try {
+    let response = await axios.get("https://pokeapi.co/api/v2/pokemon/"+endpoint);
+    console.log(figlet.textSync(response.data.name, {
+        font: 'Caligraphy',
+        horizontalLayout: 'default',
+        verticalLayout: 'default',
+        width: 200,
+        whitespaceBreak: true
+    }));
+} catch(error) {
+    console.log(error);
+}
